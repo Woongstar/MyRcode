4fbc6979324af79d4b080b370880f6b5d1d76b80
Jiwoong Yang <qualtypo@gmail.com>
2021-04-13 23:24:20 +0900

    update R code

--- 그래프 그리기.R
+++ 그래프 그리기.R
@@ -20,3 +20,13 @@ scatter <- ggplot(examData, aes(Anxiety,Exam, color=Gender))
 scatter + geom_point() + labs(x="시험 불안",y="시험 성적적")+ geom_smooth(method="lm", color="red",fill="Blue",alpha=0.15)
 scatter + geom_point() + labs(x="시험 불안",y="시험 성적적")
 #alpha 는 투명도, fill 은 se 라인 색깔, se=F SE구간 안보이기
+#히스토그램
+festivalHistogram <- ggplot(festivalData, aes(day1))
+#범례 지우기
++ theme(legend.position = "none")
+festivalHistogram + geom_histogram(binwidth = 0.4) + labs(x = "Hygiene(day1 of festival)", y="Frequency")
+# 상자수염그림
+festivalBoxplot <- ggplot(festivalData, aes(gender,day1))
+festivalBoxplot + geom_boxplot()
+#범례
+legend("topright", levels(iris$Species), pch = 1:3, bg="white")
